e27d0e828443dff0aa6697a630e63347645a0a2a
istrav <travis.burandt@gmail.com>
2022-09-04 11:32:44 -0500

    index.ts 5

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { FleetOptimizer } from './lib/fleet-optimizer'
 import * as database from './database/index'
 
-export default function main() {
-  return {
-    FleetOptimizer: FleetOptimizer,
-    database: database
-  }
+let result = {
+  FleetOptimizer: FleetOptimizer,
+  database: database
 }
+
+export default result
