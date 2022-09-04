8b7767a6b8b23b02d18f2596bcd5fb5a6556cd67
istrav <travis.burandt@gmail.com>
2022-09-04 11:35:38 -0500

    index.ts 6

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { FleetOptimizer } from './lib/fleet-optimizer'
 import * as database from './database/index'
 
 let result = {
-  FleetOptimizer: FleetOptimizer,
+  main: FleetOptimizer,
   database: database
 }
 
